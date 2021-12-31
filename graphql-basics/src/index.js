bb1e55254eafb588450b2ba15b684088997fbc9d
Moto Sakanosita <moto@sakanosita.org>
2021-12-31 22:54:21 +0900

    chore

--- graphql-basics/src/index.js
+++ graphql-basics/src/index.js
@@ -1,6 +1,41 @@
-import myCurrentLocation, { getGreeting, message, name } from "./myModule"
+import { GraphQLServer } from 'graphql-yoga'
 
-console.log(message)
-console.log(name)
-console.log(myCurrentLocation)
-console.log(getGreeting('Jessica'))
+// Scalar types - String, Boolean, Int, Float, ID
+
+
+// Type definitions (schema)
+const typeDefs = `
+    type Query {
+        me: User!
+    }
+
+    type User {
+        id: ID!
+        name: String!
+        email: String!
+        age: Int
+    }
+`
+
+// Resolver
+const resolvers = {
+    Query: {
+        me() {
+            return {
+                id: 'abc123',
+                name: 'Moto Sakanosita',
+                email: 'moto@example.com',
+                age: 43
+            }
+        }
+    }
+}
+
+const server = new GraphQLServer({
+    typeDefs,
+    resolvers
+})
+
+server.start(() => {
+    console.log('The server is up!')
+})
